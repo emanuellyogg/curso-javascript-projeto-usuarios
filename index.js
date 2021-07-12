57ab3656bba727cd9f8516ba0190292160020fbc
unknown <manuogg90@gmail.com>
2021-07-12 11:38:00 -0300

    feat: G4 - Variáveis e Seletores

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var nome = document.querySelector('#exampleInputName');
-
-nome.value = 'Emanuelly Ogg';
-nome.style.color = 'pink';
-
-var masc = document.querySelector('#exampleInputGenderM');
-var fem = document.querySelector('#exampleInputGenderF');
+var name = document.querySelector('#exampleInputName');
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birth = document.querySelector('#exampleInputBirth');
+var country = document.querySelector('#exampleInputCountry');
+var email = document.querySelector('#exampleInputEmail');
+var password = document.querySelector('#exampleInputPassword');
+var photo = document.querySelector('#exampleInputFile');
+var admin = document.querySelector('#exampleInputAdmin');
